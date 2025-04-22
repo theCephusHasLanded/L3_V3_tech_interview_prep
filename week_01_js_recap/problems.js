47431b1d1ad23bd70ed62a4eed8ada64fa69b81f
christinacephus@pursuit.org <christinacephus@pursuit.org>
2025-04-22 14:55:05 -0400

    Implement JavaScript functions for string reversal, max finding, and palindrome checking

--- week_01_js_recap/problems.js
+++ week_01_js_recap/problems.js
@@ -1,16 +1,16 @@
 // Reverse a string
 function reverseString(str) {
-
+    return str.split("").reverse().join("");
 }
-  
+
 // Find max in array
 function findMax(arr) {
-
+    return Math.max(...arr)
 }
 
 // Check if palindrome
 function isPalindrome(word) {
-    
+    return word === word.split("").reverse().join("")
 }
 
 module.exports = { reverseString, findMax, isPalindrome };
