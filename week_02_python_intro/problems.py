7d38c5b61dd64f89bdfd532ab03badb17df05d25
christinacephus@pursuit.org <christinacephus@pursuit.org>
2025-05-06 22:08:41 -0400

    All Tests Pass

--- week_02_python_intro/problems.py
+++ week_02_python_intro/problems.py
@@ -1,12 +1,11 @@
 def reverse_string(s):
-    return s[q::-1]
+    return s[::-1]
 
 def find_max(lst):
-passif not lst:
-    return None
+    if not lst:
+        return None
     return max(lst)
 
 def is_palindrome(word):
-    //solve for possible case sensitivity
     word = ''.join(c.lower() for c in word if c.isalnum())
     return word == word[::-1]
--- week_02_python_intro/test_problems.py
+++ week_02_python_intro/test_problems.py
@@ -1,12 +1,50 @@
 import pytest
 from problems import reverse_string, find_max, is_palindrome
 
-def test_reverse_string():
-    assert reverse_string("hello") == "olleh"
+class TestReverseString:
+    test_cases = [
+        ("hello", "olleh"),
+        ("python", "nohtyp"),
+        ("", ""),
+        ("a", "a"),
+        ("12345", "54321")
+    ]
 
-def test_find_max():
-    assert find_max([1, 5, 3, 9]) == 9
+    @pytest.mark.parametrize("input_str, expected", test_cases)
+    def test_reverse_string(self, input_str, expected):
+        assert reverse_string(input_str) == expected
 
-def test_is_palindrome():
-    assert is_palindrome("racecar")
-    assert not is_palindrome("hello")
+class TestFindMax:
+    test_cases = [
+        ([1, 5, 3, 9], 9),
+        ([-10, -5, 0, 5, 10], 10),
+        ([0], 0),
+        ([-1, -2, -3], -1),
+        ([], None)
+    ]
+
+    @pytest.mark.parametrize("input_list, expected", test_cases)
+    def test_find_max(self, input_list, expected):
+        assert find_max(input_list) == expected
+
+class TestIsPalindrome:
+    test_cases = [
+        ("racecar", True),
+        ("hello", False),
+        ("", True),
+        ("A man a plan a canal Panama", True),
+        ("Race Car", True),
+        ("No lemon, no melon", True),
+        ("Not a palindrome", False),
+        ("12321", True),
+        ("123", False)
+    ]
+
+    @pytest.mark.parametrize("input_word, expected", test_cases)
+    def test_is_palindrome(self, input_word, expected):
+        assert is_palindrome(input_word) == expected
+
+if __name__ == '__main__':
+    import sys
+    import pytest
+    sys.exit(pytest.main(['-v', __file__]))
